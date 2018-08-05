45d4595e917e245aeb406c4d03c8c9bd1fb60d42
wei <wwang107@gmail.com>
2018-08-05 14:59:02 +0800

    inti

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchBar from '../container/SearchBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React-Redux Simple Starter!
+      </div>
+    );
+  }
+}
+
+export default App;
